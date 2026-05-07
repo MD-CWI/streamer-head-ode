564039b94d0df54c6bb7648a5dfa246bcc9bd356
ThomasSmits <thomas.smits@cwi.nl>
2026-05-07 13:40:37 +0200

    Add negative streamers and diffusion

--- ODE_model.py
+++ ODE_model.py
@@ -26,11 +26,15 @@ p.add_argument('-Q', type=float, default=0.5,
                help='Charge (C) in units of (4 * pi * eps0) Coulombs')
 p.add_argument('-v', type=float, default=7e4,
                help='Velocity (m/s)')
-g = p.add_mutually_exclusive_group()
-g.add_argument('-R', type=float,
+p.add_argument('-R', type=float,
                help='Radius to obtain (m)')
-g.add_argument('-E_max', type=float,
+p.add_argument('-E_max', type=float,
                help='Maximal electric field to obtain (V/m)')
+#g = p.add_mutually_exclusive_group()
+#g.add_argument('-R', type=float,
+#               help='Radius to obtain (m)')
+#g.add_argument('-E_max', type=float,
+#               help='Maximal electric field to obtain (V/m)')
 p.add_argument('-fixed_I_ph', type=float,
                help='Use fixed value for photoionization source')
 p.add_argument('-E_bg', type=float, default=4.5e5,
@@ -73,6 +77,10 @@ p.add_argument('-mu_table', type=str,
 #> ===================================================================
 p.add_argument('-neg_streamer', action='store_true',
                help='simulate negative streamer head')
+p.add_argument("-use_diffusion", action='store_true',
+               help='Include diffusion constant to the models')
+p.add_argument('-dif_table', type=str,
+                help='File with electron diffusion coefficient D')
 #> ===================================================================
 args = p.parse_args()
 
@@ -83,15 +91,20 @@ N0 = args.p * 1.0e5 / (c.k * args.T)
 Td_to_SI = 1e-21 * N0
 
 # Minimal z-coordinate to consider
-z_min = 1e-5 #-5
+z_min = 1e-5
 
 # Indices of variables
 i_ne, i_ni, i_q = 0, 1, 2
+if args.use_diffusion:
+    i_w = 3
 
 # Load transport data files
 TD_alpha = np.loadtxt(args.alpha_table, skiprows=2).T
 TD_eta = np.loadtxt(args.eta_table, skiprows=2).T
 TD_mu = np.loadtxt(args.mu_table, skiprows=2).T
+#> ===================================================================
+TD_D = np.loadtxt(args.dif_table, skiprows=2).T
+#> ===================================================================
 
 # Construct functions to interpolate transport data
 f_alpha = interp1d(Td_to_SI * TD_alpha[0], TD_alpha[1]*N0,
@@ -100,6 +113,10 @@ f_eta = interp1d(Td_to_SI * TD_eta[0], TD_eta[1]*N0,
                  fill_value='extrapolate')
 f_mu = interp1d(Td_to_SI * TD_mu[0], TD_mu[1]/N0,
                 fill_value='extrapolate')
+#> ===================================================================
+f_dif = interp1d(Td_to_SI * TD_D[0], TD_D[1]/N0,
+                 fill_value='extrapolate')
+#> ===================================================================
 
 # Apply filter when computing dmu/dE
 TD_dmu_dE = np.gradient(TD_mu[1]/N0, Td_to_SI * TD_mu[0])
@@ -108,10 +125,19 @@ f_dmu_dE = interp1d(Td_to_SI * TD_mu[0], TD_dmu_dE,
                     bounds_error=False,
                     fill_value=(TD_dmu_dE[0], TD_dmu_dE[-1]))
 
+#> ===================================================================
+# Calculate diffusion coefficients
+TD_dD_dE = np.gradient(TD_D[1]/N0, Td_to_SI * TD_D[0])
+TD_dD_dE = savgol_filter(TD_dD_dE, 15, 2)
+
+f_dD_dE = interp1d(Td_to_SI * TD_D[0], TD_dD_dE,
+                   bounds_error=False,
+                   fill_value=(TD_dD_dE[0], TD_dD_dE[-1]))
+#> ===================================================================
+
 # Determine critical field
 sol = root_scalar(lambda E: f_alpha(E) - f_eta(E), bracket=[0., 1e7])
 E_breakdown = sol.root
-#E_breakdown = 1.8e6 
 
 # For numerical reasons, it is convenient if numbers are around unity.
 # Therefore we use this scaling factor. I_ph represents the number of photons
@@ -122,25 +148,39 @@ I_ph_fac = 1e15
 R0_scale = 1e-3
 E_max_scale = 1e7
 
+# We add
+v_scale = 1e5
+Q_scale = 1e-3
+
 # Initial condition
 if args.neg_streamer:
+    if args.E_max is not None:
+        args.E_max = -args.E_max
     args.E_bg = -args.E_bg
     args.Q = -args.Q
 else:
     args.E_bg = args.E_bg
     args.Q = args.Q
-y0 = np.zeros(3)
-if args.ni_bc is not None:
-    y0[[i_ne, i_ni, i_q]] = [args.ne_bc, args.ni_bc, args.Q]
+
+if args.use_diffusion:
+    y0 = np.zeros(4)
+    if args.ni_bc is not None:
+        y0[[i_ne, i_ni, i_q, i_w]] = [args.ne_bc, args.ni_bc, args.Q, 0.0]
+    else:
+        y0[[i_ne, i_ni, i_q, i_w]] = [args.ne_bc, args.ne_bc, args.Q, 0.0]
 else:
-    y0[[i_ne, i_ni, i_q]] = [args.ne_bc, args.ne_bc, args.Q]
+    y0 = np.zeros(3)
+    if args.ni_bc is not None:
+        y0[[i_ne, i_ni, i_q]] = [args.ne_bc, args.ni_bc, args.Q]
+    else:
+        y0[[i_ne, i_ni, i_q]] = [args.ne_bc, args.ne_bc, args.Q]
 
 def get_E(z, q, E_bg):
     """Get electric field"""
-    return E_bg + q/z**2
+    return E_bg + q/z**2 
 
 
-def get_dE_dz(z, q, dq_dz):
+def get_dE_dz(z, q, dq_dz, E_bg):
     """Spatial derivative of electric field"""
     return dq_dz/z**2 - 2 * q/z**3
 
@@ -148,11 +188,8 @@ def get_dE_dz(z, q, dq_dz):
 def get_z_crit(Q, E_bg):
     """Solve for location of breakdown field, but avoid negative charge"""
     Q_nonneg = np.maximum(Q, 0.)
-    if args.neg_streamer:
-        if args.R is not None:
-            return args.R
-        else:
-            return 5e-4
+    if args.R is not None and args.E_max is None:
+        return args.R
     else:
         return np.sqrt(Q_nonneg / (E_breakdown - args.E_bg))
 
@@ -181,12 +218,14 @@ def get_S_ph(z, I_ph, R):
 def ODE_model_rhs(z, y, I_ph, R):
     """Right-hand side of the ODE model"""
     dy = np.zeros_like(y)
-    ne, ni, q = y
+    if args.use_diffusion:
+        ne, ni, q, w = y
+    else:
+        ne, ni, q = y
+
     if args.neg_streamer:
-        q = q
         rho_eps = (-ni + ne) * c.e / c.epsilon_0
     else:
-        q = q
         rho_eps = (ni - ne) * c.e / c.epsilon_0
     
     E = get_E(z, q, args.E_bg)
@@ -194,28 +233,43 @@ def ODE_model_rhs(z, y, I_ph, R):
     
     alpha, eta = f_alpha(E_abs), f_eta(E_abs)
     mu, dmu_dE = f_mu(E_abs), f_dmu_dE(E_abs)
-
+    D, dD_dE   = f_dif(E_abs), f_dD_dE(E_abs)
+    
     S_ph = get_S_ph(z, I_ph, R) if I_ph > 0 else 0.
-    src = (alpha - eta) * mu * E_abs * ne + S_ph + 1.e-20
+    src = (alpha - eta) * mu * E_abs * ne + S_ph
 
     dq_dz = rho_eps * z**2
-    dE_dz = get_dE_dz(z, q, dq_dz)
+    dE_dz = get_dE_dz(z, q, dq_dz, args.E_bg)
     dmu_dz = dmu_dE * dE_dz
+    dD_dz = dD_dE * dE_dz
 
     # d/dz [ne, ni, q]
-    if args.neg_streamer:
+    if args.neg_streamer and not args.use_diffusion:
+        # Negative streamers
         dy[i_ne] = -1 / (args.v - mu * E) * (src - dmu_dz * E * ne -
                                              mu * rho_eps * ne) 
         dy[i_ni] = -src / args.v
         dy[i_q] = dq_dz
-    else:
+    elif not args.neg_streamer and not args.use_diffusion:
+        # Postive streamers
         dy[i_ne] = -1 / (args.v + mu * E) * (src + dmu_dz * E * ne +
                                              mu * rho_eps * ne)
         dy[i_ni] = -src / args.v 
         dy[i_q] = dq_dz
-
+    elif args.use_diffusion:
+        #"Include diffusion"
+        if args.neg_streamer:
+            dy[i_w] = 1/D * ((args.v + dD_dz - mu * E) * w - ne * E * dmu_dz - 
+                                                    mu * ne * rho_eps + src)
+        else:
+            dy[i_w] = -1/D * ((args.v + dD_dz - mu * E) * w - ne * E * dmu_dz -
+                                                    mu * ne * rho_eps + src)
+        dy[i_ne] = w
+        dy[i_ni] = -src / args.v
+        dy[i_q] = dq_dz
+    else:
+        sys.exit("Je moet wel wat kiezen")
     return dy
-
 
 def get_radius(sol):
     """Determine the radius for an ODE solution"""
@@ -243,6 +297,7 @@ def solve_fixed_I_ph(y0, I_ph, R=args.R):
     y0[i_q] = abs(y0[i_q])
     z_crit = get_z_crit(y0[i_q], args.E_bg)
     z_max = max(args.L_factor * z_crit, 2 * z_min)
+
     # We need high accuracy here to later do root finding on solutions
     sol = solve_ivp(ODE_model_rhs, [z_max, z_min],
                     y0, dense_output=True,
@@ -250,6 +305,7 @@ def solve_fixed_I_ph(y0, I_ph, R=args.R):
                     first_step=1e-6,
                     args=(I_ph, R), method='RK45')
     return sol
+
 
 def residual_photoi(x0, y0):
     """Return difference between guess and updated guess for I_ph and R"""
@@ -305,21 +361,54 @@ def residual_photoi_E_max(x0, y0):
                 (E_max - args.E_max)/E_max_scale]
     return residual
 
+def residual_v_Q(x, y0):
+    """
+    Solve for velocity and Q
+    while matching R and E_max.
+    """
+    
+    # Update parameters
+    args.v = x[0] * v_scale
+    y0[i_q] = x[1] * Q_scale
+    I_ph = args.fixed_I_ph
+
+    # Solve ODE
+    sol = solve_fixed_I_ph(y0, I_ph)
+    R = get_radius(sol)
+    E_max = get_E_max(sol)
+    residual = [(R - args.R) / R0_scale, (E_max - args.E_max) / E_max_scale]
+    return residual
 
 def check_convergence(sol):
-    if np.abs(sol.fun).max() > 1e-2:
+    if np.abs(sol.fun).max() > 1e-2: #9e-2: # --> Aangepast
         sys.stderr.write(str(sol) + '\n' + str(args))
         raise ValueError('No convergence, adjust numerical parameters')
     return
 
 
-if args.R is not None:
+if args.R is not None and args.E_max is not None:
+    print("Solving for v")
+    x0 = (args.v / v_scale, args.Q / Q_scale)
+    tmp = root( residual_v_Q, x0=x0, args=(y0,), method='lm', tol=args.rtol,
+        options={'eps': 1e-2, 'factor': 0.1, 'maxiter': 500, 'xtol': 1e-8, 'ftol': 1e-8})
+
+    # Recover physical variables
+    args.v = tmp.x[0] * v_scale
+    y0[i_q] = tmp.x[1] * Q_scale
+
+    I_ph = args.fixed_I_ph
+    sol = solve_fixed_I_ph(y0, I_ph)
+
+elif args.R is not None:
     # Solve iterative problem to find solution with given radius
     if args.fixed_I_ph is not None:
         # Fixed photoionization source, determine Q
-        a, b = 5e1*args.Q, -5e-1*args.Q
-        #print(a, b)
-        #print(residual_R(a, y0, args.fixed_I_ph), residual_R(b, y0, args.fixed_I_ph))
+        #> ##################################################
+        a, b = 30e1*args.Q, 9e-1*args.Q # --> Dit werkt echt matig adhoc
+        #a, b = 26.6e-1*args.Q, 1.6e-1*args.Q
+        print(residual_R(a, y0, args.fixed_I_ph), residual_R(b, y0, args.fixed_I_ph))
+        #> ##################################################
+        
         tmp = root_scalar(residual_R, args=(y0, args.fixed_I_ph),
                           x0=y0[i_q], bracket=[a,b],
                           rtol=args.rtol)
@@ -343,12 +432,12 @@ if args.R is not None:
 elif args.E_max is not None:
     if args.fixed_I_ph is None:
         Q_guesses = np.array([1.0, 0.2, 5, 0.04, 25.]) * args.Q
-
         for Q in Q_guesses:
-            y0[i_q] = Q
+            y0[i_q] = Q 
             tmp = root(residual_photoi_E_max, x0=(1., 5e-4, y0[i_q]),
-                       args=(y0, ), method='lm', tol=args.rtol,
-                       options={'eps': 5e-3})
+                       args=(y0, ), method='lm', 
+                       tol=args.rtol,
+                       options={'eps': 5e-3}) #options={'eps': 5e-3}
             if np.abs(tmp.fun).max() < 1e-2:
                 break
         check_convergence(tmp)
@@ -357,8 +446,14 @@ elif args.E_max is not None:
         sol = solve_fixed_I_ph(y0, I_ph, R)
     elif args.fixed_I_ph == 0.0:
         # Fixed photoionization source, determine Q
+        #> ##################################################
+        #a, b = 5e-2*args.Q, 5e-2*args.Q # --> Dit werkt echt matig adhoc
+        a, b = 30e1*args.Q, 9e-1*args.Q
+        print(residual_E_max(a, y0, args.fixed_I_ph, 0.), residual_E_max(b, y0, args.fixed_I_ph, 0.))
+        #> ##################################################
+
         tmp = root_scalar(residual_E_max, args=(y0, args.fixed_I_ph, 0.0),
-                          x0=y0[i_q], bracket=[1e-2*args.Q, 1e4*args.Q],
+                          x0=y0[i_q], bracket=[a,b],
                           rtol=args.rtol)
         I_ph = args.fixed_I_ph
         y0[i_q] = tmp.root
@@ -390,10 +485,14 @@ if args.compact_output:
     print(f'{args.v:.3e} {R:.3e} {y0[i_q]:.3e} {args.E_bg:.3e} ' +
           f'{E_max:.3e} {n_ch:.3e} {current_ratio:.3e} {args.ne_bc:.3e}')
 else:
-    if args.neg_streamer:
-        print(f'Simulate a negative streamer:')
+    if args.neg_streamer and args.use_diffusion:
+        print(f'Simulate a negative streamer with diffusion:')
+    elif args.neg_streamer and not args.use_diffusion:
+        print(f'Simulate a negative streamer without diffusion:')
+    elif not args.neg_streamer and args.use_diffusion:
+        print(f'Simulate a postive streamer with diffusion:')
     else:
-        print(f'Simulate a postive streamer:')
+        print(f'Simulate a postive streamer without diffusion:')
     print(f'Velocity [m/s]:         {args.v:.3e}')
     print(f'E_bg [V/m]:             {args.E_bg:.3e}')
     print(f'Channel density [m^-3]: {n_ch:.3e}')
